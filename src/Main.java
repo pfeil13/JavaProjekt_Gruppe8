5af6a21e74f8fca3f74dc51b44e9ca6f9e31c696
jpfei <pfeilj@dhbw-loerrach.de>
2019-02-15 11:07:53 +0100

    Menue hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,63 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+
+        Menue.menue();//Menüaufruf
+
+
+
+    }
+}
+
+
+class Menue
+{
+    public static void menue()
+    {
         Scanner scanner = new Scanner(System.in);
-        //Menüaufruf
 
         System.out.println("Herzlich Willkommen zu unserem Quiz.");
         System.out.println("Verrate uns doch noch deinen Namen: ");
+        String name= scanner.next();
+
+        System.out.println("Hallo "+name+", was möchtest du machen? \n Quiz starten(1)  Tipps/Hilfe(2)  Themengebiete(3)");
+        int auswahl=scanner.nextInt();
+
+        if (auswahl==1)
+        {
+            Quiz.quiz();
+        }
+        else if(auswahl==2)
+        {
+            Hilfe.hilfe();
+        }
+        else
+        {
+            Themengebiete.themengebiete();
+        }
+    }
+
+}
+
+class Quiz
+{
+    public static void quiz()
+    {
+       System.out.println("Test");
+    }
+}
+
+class Hilfe{
+    public static void hilfe()
+    {
+        System.out.println("Test");
+    }
+}
+
+class Themengebiete{
+    public static void themengebiete()
+    {
+        System.out.println("Test");
     }
 }
 
