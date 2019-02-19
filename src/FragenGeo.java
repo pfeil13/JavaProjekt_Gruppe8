10618ed27090924e23e0617be635789b3698d062
Tobi1495 <hessto@dhbw-loerrach.de>
2019-02-19 15:17:50 +0100

    Optimierung

--- src/FragenGeo.java
+++ src/FragenGeo.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 class FragenGeo {
     public static void main(String[] args) throws IOException {
