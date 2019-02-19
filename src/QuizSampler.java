48870d54003e8a22bffe63c6a717f9d7ba8a3ce9
Tobi1495 <hessto@dhbw-loerrach.de>
2019-02-19 10:22:11 +0100

    Klasse Quiz als "QuizSampler" umbenannt um Klarheit zu schaffen.

--- src/QuizSampler.java
+++ src/QuizSampler.java
@@ -1,4 +1,4 @@
-public class Quiz {
+public class QuizSampler {
     public static void main(String[] args)  {
         Questions quiz = new Questions();
         for (Question question: quiz.getQuestions()) {
