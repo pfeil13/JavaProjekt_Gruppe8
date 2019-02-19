704989913957bedc2b40c5d5efbbe3eaff553a79
Tobi1495 <hessto@dhbw-loerrach.de>
2019-02-19 14:12:49 +0100

    Keine "Übersetzung" von Str. in Int mehr nötig. Wurde durch "equals" gelöst.

--- src/Main.java
+++ src/Main.java
@@ -17,12 +17,11 @@ class Menue {
 
     public static void menue(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Herzlich Willkommen zu unserem Quiz."); //TODO: Auslagern der Namensfeststellung, damit man nicht immer den Namen neu eingeben muss, sobald man beim Themengebiet auf "zurück" geht. Damit will man ja nur das Menü neu aufrufen und nicht nochmal seinen Namen eingeben!
         System.out.println("Verrate uns doch noch deinen Namen: ");
-        String name = scanner.next();
+        String username = scanner.next();
 
-        System.out.println("Hallo " + name + ", was möchtest du machen? \n Quiz starten(1)  Hilfe(2)");
+        System.out.println("Hallo " + username + ", was möchtest du machen? \n Quiz starten(1)  Hilfe(2)");
         int auswahl;
 
         do {
@@ -50,6 +49,55 @@ class Menue {
 
 }
 
+//Die Fragen der einzelnen Themenbereiche (Themenquiz)
+
+class FragenGeo {
+    public static void main(String[] args) throws IOException {
+        FileReader fr = new FileReader("src/geo_fragen2.txt");
+        BufferedReader br = new BufferedReader(fr);
+        String zeile_x = "0";
+        int richtig = 0;
+        int falsch = 0;
+        int anzahlfragen = 0;
+
+
+        while (!zeile_x.equals("###")) {
+            zeile_x = br.readLine();
+            if (zeile_x.equals("###"))
+            {
+                anzahlfragen=richtig+falsch;
+                System.out.println("Du hast von " + anzahlfragen + " Fragen " + richtig + " Fragen korrekt beantwortet.");
+                break;
+            }
+            System.out.println(zeile_x);
+            zeile_x = br.readLine();
+            System.out.println(zeile_x);
+            zeile_x = br.readLine();
+            System.out.println(zeile_x);
+            zeile_x = br.readLine();
+            System.out.println(zeile_x);
+            zeile_x = br.readLine();
+            System.out.println(zeile_x);
+            String antwort = br.readLine();
+
+            System.out.println("Deine Antwort: ");
+            Scanner scanner = new Scanner(System.in);
+            String eingabe = scanner.nextLine();
+
+            if (eingabe.equals(antwort)) {
+                System.out.println("Das war die richtige Antwort!");
+                richtig++;
+            } else if (!eingabe.equals(antwort)) {
+                System.out.println("Das war leider falsch!");
+                falsch++;
+            }
+        }
+
+        br.close();
+    }
+}
+
+// Die Anleitung
 class Hilfe {
     public static void main(String[] args) throws IOException {
         FileReader freader = new FileReader("src/anleitung");
@@ -74,45 +122,5 @@ class Hilfe {
             System.out.println("Diese Option ist nicht verfügbar!");
         Hilfe.main(args);
         breader.close();
-    }
-}
-
-class FragenGeo {
-    public static void main(String[] args) throws IOException {
-        FileReader fr = new FileReader("src/geo_fragen2.txt");
-        BufferedReader br = new BufferedReader(fr);
-        String zeile_x = "0";
-
-        while (!zeile_x.equals("###")) {
-            zeile_x = br.readLine();
-            if (zeile_x.equals("###"))
-            {
-                break;
-            }
-            System.out.println(zeile_x);
-            zeile_x = br.readLine();
-            System.out.println(zeile_x);
-            zeile_x = br.readLine();
-            System.out.println(zeile_x);
-            zeile_x = br.readLine();
-            System.out.println(zeile_x);
-            zeile_x = br.readLine();
-            System.out.println(zeile_x);
-            String str_antwort = br.readLine();
-            Integer int_antwort = Integer.valueOf(str_antwort);
-
-            System.out.println("Deine Antwort: ");
-            Scanner scanner = new Scanner(System.in);
-            String str_eingabe = scanner.nextLine();
-            Integer int_eingabe = Integer.valueOf(str_eingabe);
-
-            if (int_eingabe == int_antwort) {
-                System.out.println("Das war die richtige Antwort!");
-            } else if (int_eingabe != int_antwort) {
-                System.out.println("Das war leider falsch!");
-            }
-        }
-
-        br.close();
     }
 }
