bcf97443b440646a5702dc69d8dd021f387fc9f6
Tobi1495 <Tobias.Hess2@gmx.de>
2019-02-27 01:24:08 +0100

    Datei beschreiben I

--- src/WriteCatalog.java
+++ src/WriteCatalog.java
@@ -1,3 +1,16 @@
-public class WriteCatalog {
+import java.util.*;
+import java.io.*;
 
+public class WriteCatalog {
+    public static void main(String[] args) throws IOException {
+
+        FileWriter fw = new FileWriter("src_usercatalog.txt");
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        bw.write("test test test");
+        bw.write("tset tset tset");
+
+        bw.close();
+
+    }
 }
