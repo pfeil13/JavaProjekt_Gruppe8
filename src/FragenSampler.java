d27fb09ecda082fa2d23cdfc6b6b6282a001a292
Tobi1495 <Tobias.Hess2@gmx.de>
2019-03-01 21:42:56 +0100

    Bugfix: Plural/Singular Problem gelöst

--- src/FragenSampler.java
+++ src/FragenSampler.java
@@ -21,13 +21,15 @@ class FragenSampler {
             //Pfad
             path = begin + "geschichte" + ending;
         } else if (userInput == 4) {
-            //TODO: Zufallsmodus!!!
+
         } else if (userInput == 5) {
             path = beginUM + "user_catalog" + ending; //UM stehht für UserMode
         }
 
         FileReader fr = new FileReader(path);
         BufferedReader br = new BufferedReader(fr);
+        String textmodule_correctQuestions = "";
+        String textmodule_amountOfQuestions = "";
         String line_x = "0";
         int correct = 0;
         int wrong = 0;
@@ -43,7 +45,20 @@ class FragenSampler {
                 amountOfQuestions = correct + wrong;
 
                 do {
-                    System.out.println(name + " du hast von " + amountOfQuestions + " Fragen " + correct + " Fragen korrekt beantwortet.\n");
+                    if (correct == 1) {
+                        textmodule_correctQuestions = "Frage";
+                    } else if (correct > 1) {
+                        textmodule_correctQuestions = "Fragen";
+                        textmodule_amountOfQuestions = "Fragen";
+                    } else if (correct == 0) {
+                        textmodule_correctQuestions = "Fragen";
+                    }
+                    if (amountOfQuestions > 1) {
+                        textmodule_amountOfQuestions = "Fragen";
+                    } else if (amountOfQuestions <= 1) {
+                        textmodule_amountOfQuestions = "Frage";
+                    }
+                    System.out.println(name + " du hast von " + amountOfQuestions + " " + textmodule_amountOfQuestions + " " + correct + " " + textmodule_correctQuestions + " korrekt beantwortet.\n");
                     System.out.println("Was möchtest du jetzt tun? \n Zurück zum Menü(1)");
                     userInput = scanner.nextInt();
 
